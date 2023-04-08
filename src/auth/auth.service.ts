a8f36772dd2a8a5395e119c3b70d57f29f0e4c23
khier.talia <talia.khier-ext@korian.fr>
2023-04-09 00:19:39 +0100

    setup a login/signup service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signin() {
+    return {
+      msg: 'This is the signin route',
+    };
+  }
+  signup() {
+    return {
+      msg: 'This is the signup route',
+    };
+  }
+}
