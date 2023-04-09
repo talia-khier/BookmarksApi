28a511c75f29bc2084dadb95dd34b5fa770efb21
khier.talia <talia.khier-ext@korian.fr>
2023-04-09 17:47:24 +0100

    update prisma test script

--- scripts/script.ts
+++ scripts/script.ts
@@ -4,12 +4,12 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  const user = await prisma.user.create({
-    data: {
-      email: 'khiertalia1@gmail.com',
-    },
-  });
-  console.log(user);
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email: 'khiertalia1@gmail.com',
+  //   },
+  // });
+  // console.log(user);
 }
 
 main()
