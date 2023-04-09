735787377dd1f5bd482c562426c46b86d8588ace
khier.talia <talia.khier-ext@korian.fr>
2023-04-09 22:51:36 +0100

    add request decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
-    return this.authService.signup();
+  signup(@Req() req: Request) {
+    console.log(req.body);
+    return this.authService.signup(req.body);
   }
 
   @Post('signin')
