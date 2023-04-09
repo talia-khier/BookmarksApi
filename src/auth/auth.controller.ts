383929a3b2fca1c13a6e71998cf6de17830a055b
khier.talia <talia.khier-ext@korian.fr>
2023-04-09 23:10:29 +0100

    use barrel export pattern

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AUthDTO } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: any) {
+  signup(@Body() dto: AUthDTO) {
     console.log(dto);
     return this.authService.signup();
   }
