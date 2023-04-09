6e9909a27624a500fe97620d687ae2f9967e6c3d
khier.talia <talia.khier-ext@korian.fr>
2023-04-09 22:57:19 +0100

    replace @Req with @Body

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,14 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Req() req: Request) {
-    console.log(req.body);
-    return this.authService.signup(req.body);
+  signup(@Body() dto: any) {
+    console.log(dto);
+    return this.authService.signup();
   }
 
   @Post('signin')
