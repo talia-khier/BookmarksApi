6b87f15ee48ef0a0fb8c5eed11123ddbc5a85281
khier.talia <talia.khier-ext@korian.fr>
2023-04-09 22:43:31 +0100

    create prisma module/ service and make it global

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { User, Bookmarks } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signin() {
     return {
       msg: 'This is the signin route',
