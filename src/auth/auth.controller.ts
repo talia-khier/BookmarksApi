01d3565a2536c19d2aeb7633f00cb2b092ee533d
khier.talia <talia.khier-ext@korian.fr>
2023-04-13 01:40:00 +0100

    implement signup logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AUthDTO } from './dto';
+import { AuthDTO } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AUthDTO) {
-    console.log(dto);
-    return this.authService.signup();
+  signup(@Body() dto: AuthDTO) {
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { User, Bookmarks } from '@prisma/client';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDTO } from './dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDTO) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // save the user to the database
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+
+      delete user.hash;
+      return user;
+    } catch (e) {
+      console.log(e);
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('User already exists');
+        }
+      }
+    }
+  }
   signin() {
     return {
       msg: 'This is the signin route',
-    };
-  }
-  signup() {
-    return {
-      msg: 'This is the signup route',
     };
   }
 }
