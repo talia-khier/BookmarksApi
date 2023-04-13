41a15d68d39ea3dae0fc0113f306db74af3c24e6
khier.talia <talia.khier-ext@korian.fr>
2023-04-13 02:32:44 +0100

    implement confi module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(configModule: ConfigService) {
     super({
       datasources: {
         db: {
-          url: process.env.DATABASE_URL,
+          url: configModule.getOrThrow('DATABASE_URL'),
         },
       },
     });
